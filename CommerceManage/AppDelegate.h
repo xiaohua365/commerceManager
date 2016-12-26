e2707014180971bd849dfc7bd71cc68d13511df1
保护陈小花 <baohuchenxiaohua@vaic.local>
2016-12-26 09:03:35 +0800

    Initial Commit

---
+++ CommerceManage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CommerceManage
+//
+//  Created by 小花 on 2016/12/26.
+//  Copyright © 2016年 vaic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CommerceManage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CommerceManage
+//
+//  Created by 小花 on 2016/12/26.
+//  Copyright © 2016年 vaic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
